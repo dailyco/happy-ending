80a917e5cacfdf019b4952770141e601d5c38fd6
YuJin-Kim <21700140@handong.edu>
2020-09-19 19:53:15 +0900

    P 컴포넌트 생성 - P.js: 문단 컴포넌트 생성 [update]

--- happy-ending/src/ui/Atoms/P.js
+++ happy-ending/src/ui/Atoms/P.js
@@ -1,1 +1,20 @@
-// TODO: Code here
+import React from "react";
+
+function P(text, /*width, font_size, color,*/ { children }) {
+  // TODO: create style code
+  // const style = ;
+  return (
+    <p
+    // style = {style}
+    >
+      ${text}
+      {children}
+    </p>
+  );
+}
+
+P.defaultProps = {
+  text = "",
+};
+
+export default P;
