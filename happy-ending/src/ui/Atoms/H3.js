0c46e8b99404565af14c20473c423bbdbb9abb6a
Kim Seokjin <kimseokjin@Kimui-MacBookPro.local>
2020-09-20 15:21:45 +0900

    Atom componet 중 H3, TextField 구현
    src
     L ui
       L Atoms
         L H3.js : atom compnent 중 H3 구현 [UPDATE]
         L TextField.js : atom component 중 TextFiled 구현 [UPDATE]

--- happy-ending/src/ui/Atoms/H3.js
+++ happy-ending/src/ui/Atoms/H3.js
@@ -1,1 +1,14 @@
-// TODO: Code here
+import React from "react";
+import "../../scss/Atoms/H3.scss";
+
+function H3({ text, color, pos }) {
+  return <h3 className={`Buttons ${color} ${pos}`}>{text}</h3>;
+}
+
+H3.defaultProps = {
+  text: "",
+  color: "black",
+  pos: "center",
+};
+
+export default H3;
--- happy-ending/src/ui/Atoms/TextField.js
+++ happy-ending/src/ui/Atoms/TextField.js
@@ -1,1 +1,19 @@
-// TODO: Code here
+import React from "react";
+import "../../scss/Atoms/TextField.scss";
+
+function TextField({ text, placeHolder, size, color }) {
+  return (
+    <input type="text" placeholder={placeHolder} className={`TextField ${size} ${color}`}>
+      {text}
+    </input>
+  );
+}
+
+TextField.defaultProps = {
+  text: "",
+  placeHolder: "내용을 입력해주세요",
+  size: "medium",
+  color: "gray",
+};
+
+export default TextField;
