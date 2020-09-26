4c0adb9c1eb6e03e1eebae1fb01e54b09a9649df
YuJin-Kim <21700140@handong.edu>
2020-09-27 00:57:36 +0900

    SelectionResponse 컴포넌트 생성 - SelectionResponse.js: SelectionBox 반환 [create]

---
+++ happy-ending/src/ui/Organisms/SelectionResponse.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SelectionBox from "../Molecules/SelectionBox";
+
+function SelectionResponse() {
+  return <SelectionBox></SelectionBox>;
+}
+
+export default SelectionResponse;
