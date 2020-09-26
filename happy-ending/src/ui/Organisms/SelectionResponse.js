9d4e3bbe66be26bdf264fe080a78c45ae3f27f08
YuJin-Kim <21700140@handong.edu>
2020-09-27 01:00:45 +0900

    SelectionBox, SelectionResponse props 수정 - SelectionBox.js: props로 받는 배열을 객체의 배열로 수정 [update] - SelectionResponse.js: SelectionBox에게 넘겨주는 props 생성 [update]

--- happy-ending/src/ui/Organisms/SelectionResponse.js
+++ happy-ending/src/ui/Organisms/SelectionResponse.js
@@ -1,8 +1,8 @@
 import React from "react";
 import SelectionBox from "../Molecules/SelectionBox";
 
-function SelectionResponse() {
-  return <SelectionBox></SelectionBox>;
+function SelectionResponse({ objects }) {
+  return <SelectionBox objects={objects}></SelectionBox>;
 }
 
 export default SelectionResponse;
