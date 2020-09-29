bfdd5cf01ce701a26ccf35c5aaabe27bbb7798c7
YuJin-Kim <21700140@handong.edu>
2020-09-30 02:31:12 +0900

    오타 수정 - FullTextFieldTemplate.scss: FullTextFieldTempalte -> FullTextFieldTemplate [update] - FullTextFieldTemplate.js: FullTextFieldTempalte -> FullTextFieldTemplate [update]

--- happy-ending/src/ui/Templates/FullTextFieldTemplate.js
+++ happy-ending/src/ui/Templates/FullTextFieldTemplate.js
@@ -9,7 +9,7 @@ function FullTextFieldTemplate({ data, styleName }) {
   const { dq_data, ftfr_data } = data;
   const { template_style, dq_style, ftfr_style } = styleName;
   return (
-    <div className={classNames("FullTextFieldTempalte", template_style)}>
+    <div className={classNames("FullTextFieldTemplate", template_style)}>
       <Dquestion data={dq_data} styleName={dq_style}></Dquestion>
       <FullTextFieldResponse data={ftfr_data} styleName={ftfr_style}></FullTextFieldResponse>
     </div>
