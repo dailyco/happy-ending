c6451e0d0cd3a7e022b7d32da8edc14651edee75
Kim Seokjin <kimseokjin@Kimui-MacBookPro.local>
2020-09-30 03:06:26 +0900

    Item 컴퍼넌트 중 TextField 스타일링 분석 및 구현
    colors.scss -> dim gray (#707070) 추가 [UPDATE]
    TextField.scss -> item 클래스 추가 [UPDATE]
    Item.js -> TextField에 item 클래스 추가 [UPDATE]

--- happy-ending/src/ui/Molecules/Item.js
+++ happy-ending/src/ui/Molecules/Item.js
@@ -5,7 +5,7 @@ import TextField from "../Atoms/TextField";
 function Item() {
   return (
     <>
-      <TextField placeHolder="누구"></TextField>
+      <TextField placeHolder="누구" styleName={["item"]}></TextField>
       <Span styleName={["s", "eclipse", "center"]}>에게</Span>
       <TextArea placeHolder="그동안 하지 못했던 말들을 적어보세요.(300자 제한)" styleName={["item"]}></TextArea>
     </>
