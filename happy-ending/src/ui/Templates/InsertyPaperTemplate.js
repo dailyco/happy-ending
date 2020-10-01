097933e6b7f13e36b7dec2312f80111de47607d3
YuJin-Kim <21700140@handong.edu>
2020-10-02 01:29:06 +0900

    InsertyPaperTemplate을 사용하는 페이지의 디테일한 스타일 적용 - InsertyPaperTemplate의 디테일한 스타일 적용을 위해 수정 [update] - InsertyPaperTemplate을 사용하는 페이지 P10, P11, P23, P26의 데이터값 수정 [update]

--- happy-ending/src/ui/Templates/InsertyPaperTemplate.js
+++ happy-ending/src/ui/Templates/InsertyPaperTemplate.js
@@ -6,12 +6,12 @@ import Span from "../Atoms/Span";
 import "../../scss/Templates/InsertyPaperTemplate.scss";
 
 function InsertyPaperTemplate({ data, styleName }) {
-  const { dq_data, span_data } = data;
+  const { dq_data } = data;
   const { template_style, dq_style, span_style } = styleName;
   return (
     <div className={classNames("InsertyPaperTemplate", template_style)}>
       <Dquestion data={dq_data} styleName={dq_style}></Dquestion>
-      <Span styleName={span_style}>{span_data.text}</Span>
+      <Span styleName={span_style}>탭하여 다음 페이지로 넘어가기</Span>
     </div>
   );
 }
