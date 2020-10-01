f85a75f45574c104f18b002248b8b8273427087f
Kim Seokjin <kimseokjin@Kimui-MacBookPro.local>
2020-10-01 23:55:57 +0900

    25번 페이지 구현
    Span.scss -> font-weight bold 추가 [UPDATE]

---
+++ happy-ending/src/ui/Pages/P25.js
@@ -0,0 +1,32 @@
+import React from "react";
+import SeparatedTemplate25 from "../Templates/SeparatedTemplate25";
+import P25_Video from "../../assets/video/25-death.mp4";
+import "../../scss/Pages/P25.scss";
+
+function P25() {
+  const data = {
+    h3_data: "D-DAY",
+    span1_data: "홍길동 님은",
+    span2_data: "2020.00.00",
+    span3_data: "사망했습니다.",
+    span4_data: "탭하여 다음 페이지로 넘어가기",
+  };
+  const styleName = {
+    h3_style: ["heavy", "neon-green"],
+    span1_style: ["noto", "s", "medium", "neon-green"],
+    span2_style: ["nanum", "l", "bold", "neon-green"],
+    span3_style: ["noto", "s", "medium", "neon-green"],
+    span4_style: ["noto", "xs", "light", "neon-green"],
+  };
+
+  return (
+    <div className={"P25"}>
+      <video autoPlay muted loop>
+        <source src={P25_Video} type="video/mp4" />
+      </video>
+      <SeparatedTemplate25 data={data} styleName={styleName}></SeparatedTemplate25>
+    </div>
+  );
+}
+
+export default P25;
---
+++ happy-ending/src/ui/Templates/SeparatedTemplate25.js
@@ -0,0 +1,21 @@
+import React from "react";
+import H3 from "../Atoms/H3";
+import Span from "../Atoms/Span";
+import "../../scss/Templates/SeparatedTemplate25.scss";
+
+function SeparatedTemplate25({ data, styleName }) {
+  const { h3_data, span1_data, span2_data, span3_data, span4_data } = data;
+  const { h3_style, span1_style, span2_style, span3_style, span4_style } = styleName;
+
+  return (
+    <div className={"SeparatedTemplate25"}>
+      <H3 styleName={h3_style}>{h3_data}</H3>
+      <Span styleName={span1_style}>{span1_data}</Span>
+      <Span styleName={span2_style}>{span2_data}</Span>
+      <Span styleName={span3_style}>{span3_data}</Span>
+      <Span styleName={span4_style}>{span4_data}</Span>
+    </div>
+  );
+}
+
+export default SeparatedTemplate25;
