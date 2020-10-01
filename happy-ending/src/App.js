bfdd65b6c9e729f9caf2e3c502fdfcaa2b4af82e
YuJin-Kim <21700140@handong.edu>
2020-10-01 14:57:53 +0900

    fonts, videos, images reload

--- happy-ending/src/App.js
+++ happy-ending/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import P26 from "./ui/Pages/P26";
+import P13 from "./ui/Pages/P13";
 
 import "./App.css";
 
 function App() {
-  return <P26></P26>;
+  return <P13></P13>;
 }
 
 export default App;
