14aa508629102292d1faefe46953c4c6e034e9b9
Kim Seokjin <kimseokjin@Kimui-MacBookPro.local>
2020-10-02 01:15:57 +0900

    페이지 24 구현

--- happy-ending/src/ui/Molecules/GraveStone.js
+++ happy-ending/src/ui/Molecules/GraveStone.js
@@ -5,7 +5,7 @@ import TextField from "../Atoms/TextField";
 import "../../scss/Molecules/GraveStone.scss";
 
 function GraveStone({ data, styleName }) {
-  const { tf_data, sp1_data, sp2_data, ta_data } = data;
+  const { tf_data, sp1_data, sp2_data } = data;
   const { tf_style, sp1_style, sp2_style, ta_style } = styleName;
 
   return (
@@ -13,7 +13,10 @@ function GraveStone({ data, styleName }) {
       <TextField placeHolder={tf_data} styleName={tf_style}></TextField>
       <Span styleName={sp1_style}>{sp1_data}</Span>
       <Span styleName={sp2_style}>{sp2_data.name}</Span>
-      <TextArea placeHolder={ta_data} styleName={ta_style}></TextArea>
+      <TextArea
+        placeHolder="(남기고 싶은 한마디를 적어주세요. &#13;&#10; Ex. 세상에 온기를 채우고 떠나다.)"
+        styleName={ta_style}
+      ></TextArea>
     </div>
   );
 }
