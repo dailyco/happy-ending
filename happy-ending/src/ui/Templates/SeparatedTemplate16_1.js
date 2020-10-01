557460dc8a1775df7d33905d77fc1f30e87db9d9
Kim Seokjin <kimseokjin@Kimui-MacBookPro.local>
2020-10-01 19:56:41 +0900

    SeparatedTemplate 16-2 추가 Button.scss -> circle 클래스 추가 [UPDATE] SeparatedTemplate16_1.js -> [CREATE]

---
+++ happy-ending/src/ui/Templates/SeparatedTemplate16_1.js
@@ -0,0 +1,37 @@
+import React from "react";
+import P from "../Atoms/P";
+import Webcam from "react-webcam";
+import { MdPhotoCamera } from "react-icons/md";
+import Button from "../Atoms/Button";
+import "../../scss/Templates/SeparatedTemplate16_1.scss";
+
+function SeparatedTemplate16_1({ data, styleName }) {
+  const { p_data } = data;
+  const { p_style, btn_style } = styleName;
+
+  const videoConstraints = {
+    width: { min: 480 },
+    height: { min: 720 },
+    aspectRatio: 0.6666666667,
+  };
+
+  const webcamRef = React.useRef(null);
+  const [imgSrc, setImgSrc] = React.useState(null);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef, setImgSrc]);
+
+  return (
+    <div className={SeparatedTemplate16_1}>
+      <P styleName={p_style}>{p_data.text}</P>
+      <Webcam audio={false} videoConstraints={videoConstraints}></Webcam>
+      <Button styleName={btn_style} onClick={capture}>
+        <MdPhotoCamera size={32}></MdPhotoCamera>
+      </Button>
+    </div>
+  );
+}
+
+export default SeparatedTemplate16_1;
