c83049c7d9028e0561c83e8d69e23f9e35379bd7
Kim Seokjin <kimseokjin@Kimui-MacBookPro.local>
2020-10-02 01:37:58 +0900

    SeparateTemplate 17 구현:

---
+++ happy-ending/src/ui/Templates/SeparateTemplate17.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Dquestion from "../Organisms/Dquestion";
+import Img from "../Atoms/Img";
+import "../../scss/Templates/SeparateTemplate17.scss";
+
+function SeparateTemplate17({ data, styleName }) {
+  const { dq_data, imgs } = data;
+  const { dq_style } = styleName;
+
+  return (
+    <div className={SeparateTemplate17}>
+      <Dquestion data={dq_data} styleName={dq_style}></Dquestion>
+      {imgs.map((img) => (
+        <Img src={img.src} alt={img.alt}></Img>
+      ))}
+    </div>
+  );
+}
+
+export default SeparateTemplate17;
