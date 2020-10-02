744692d53ae70155f2083aaf8b920cb091e8a2de
Kim Seokjin <kimseokjin@Kimui-MacBookPro.local>
2020-10-02 17:43:22 +0900

    페이지 6번, 8번 구현

--- happy-ending/src/ui/Templates/MultipleChoiceTemplate.js
+++ happy-ending/src/ui/Templates/MultipleChoiceTemplate.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Dquestion from "../Organisms/Dquestion";
 import SelectionResponse from "../Organisms/SelectionResponse";
+import "../../scss/Templates/MultipleChoiceTemplate.scss";
 
 function MultipleChoiceTemplate({ data, styleName }) {
   const { dq_data, sr_data } = data;
   const { dq_style, sr_style } = styleName;
 
   return (
-    <>
+    <div className={"MultipleChoiceTemplate"}>
       <Dquestion data={dq_data} styleName={dq_style}></Dquestion>
       <SelectionResponse data={sr_data} styleName={sr_style}></SelectionResponse>
-    </>
+    </div>
   );
 }
 
