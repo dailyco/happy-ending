7dedbaf78640fb3975ac7c6eaccac6216569b2b3
Kim Seokjin <kimseokjin@Kimui-MacBookPro.local>
2020-10-08 20:24:08 +0900

    레이아웃 수정 완

--- happy-ending/src/ui/Templates/SeparatedTemplate16_1.js
+++ happy-ending/src/ui/Templates/SeparatedTemplate16_1.js
@@ -19,6 +19,12 @@ function SeparatedTemplate16_1({ data, styleName }) {
 
   const webcamRef = React.useRef(null);
   const [imgSrc, setImgSrc] = React.useState(null);
+  const [counter, setCounter] = React.useState(3);
+
+  const countDown = React.useEffect(() => {
+    const timer = counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+    return () => clearInterval(timer);
+  }, [counter]);
 
   const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -26,7 +32,7 @@ function SeparatedTemplate16_1({ data, styleName }) {
   }, [webcamRef, setImgSrc]);
 
   const captureAfter3Sec = React.useCallback(() => {
-    window.setTimeout(capture, 3000);
+    setTimeout(capture, 3000);
   }, [capture]);
 
   const retake = React.useCallback(() => {
@@ -39,6 +45,7 @@ function SeparatedTemplate16_1({ data, styleName }) {
         <>
           <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" videoConstraints={videoConstraints}></Webcam>
           <P styleName={p_style}>{p_data.text}</P>
+          {/* <P styleName={p_style}>{counter}</P> */}
           <Button className={"capture"} styleName={btn_style} onClick={captureAfter3Sec}></Button>
         </>
       )}
