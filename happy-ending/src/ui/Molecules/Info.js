35ce1e2d3b57a3fc3e3fd7f6ea48b2edc6ddedbd
YuJin-Kim <21700140@handong.edu>
2020-10-13 01:13:32 +0900

    P4 apply state management & localStorage store

--- happy-ending/src/ui/Molecules/Info.js
+++ happy-ending/src/ui/Molecules/Info.js
@@ -2,35 +2,20 @@ import React from "react";
 import TextField from "../Atoms/TextField";
 import classNames from "classnames";
 
-function Info() {
-  const [inputs, setInputs] = useState({
-    n: localStorage.getItem("name"),
-    y: localStorage.getItem("year"),
-    m: localStorage.getItem("month"),
-    d: localStorage.getItem("day"),
-  });
-
-  const {n, y, m, d} = inputs;
-  const onChange = (e) => {
-    const { key, name, value } = e.target;
-
-    setInputs({
-      ...inputs,
-      [key]: value,
-    });
-    // localStorage.setItem(name, value);
-    // console.log(localStorage);
-  }
+function Info({data}) {
+  const { inputs, names, onChange } = data;
+  const { n, y, m, d } = inputs;
+  const [ name, year, month, day ] = names;
 
   return (
     <div className={classNames("Info")}>
       <div className={classNames("name")}>
-        <TextField placeHolder="이름" styleName={["m", "davy-gray, info"]} value={n} onChange={onChange} name="name"></TextField>
+        <TextField placeHolder="이름" styleName={["m", "davy-gray, info"]} value={n} onChange={onChange} name={name}></TextField>
       </div>
       <div className={classNames("birthday")}>
-        <TextField placeHolder="YY" styleName={["xs", "davy-gray, info"]} value={y} onChange={onChange} name="year"></TextField>
-        <TextField placeHolder="MM" styleName={["xs", "davy-gray, info"]} value={m} onChange={onChange} name="month"></TextField>
-        <TextField placeHolder="DD" styleName={["xs", "davy-gray, info"]} value={d} onChange={onChange} name="day"></TextField>
+        <TextField placeHolder="YY" styleName={["xs", "davy-gray, info"]} value={y} onChange={onChange} name={year}></TextField>
+        <TextField placeHolder="MM" styleName={["xs", "davy-gray, info"]} value={m} onChange={onChange} name={month}></TextField>
+        <TextField placeHolder="DD" styleName={["xs", "davy-gray, info"]} value={d} onChange={onChange} name={day}></TextField>
       </div>
     </div>
   );
