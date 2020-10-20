2ab9d472646958aafb403443d5fb2caedb993308
YuJin-Kim <21700140@handong.edu>
2020-10-20 22:26:26 +0900

    Add button animation effect #133

--- happy-ending/src/ui/Molecules/SelectionBox.js
+++ happy-ending/src/ui/Molecules/SelectionBox.js
@@ -1,17 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
 import Button from "../Atoms/Button";
 
 function SelectionBox({ data, styleName }) {
-  const { buttons } = data;
+  const history = useHistory();
+  const { to, buttons } = data;
   const { btn_style } = styleName;
+  const selectionBoxRef = useRef();
+
+  useEffect(() => {
+    selectionBoxRef.current.addEventListener('click', clickHandler)
+    return () => selectionBoxRef.current.removeEventListener('click', clickHandler)
+  }, []);
+  
+  function clickHandler(e) {
+    e.preventDefault()
+    e.target.classList.add('selected')
+    Array.from(e.currentTarget.querySelectorAll('.Button')).filter(
+      (b) => !b.classList.contains('selected')).forEach(b => b.classList.add('unselected'))
+
+    e.target.addEventListener('animationend', () => {
+      history.push(to)
+    })
+  }
 
   return (
-    <div className={"SelectionBox"}>
+    <div className={"SelectionBox"} ref={selectionBoxRef}>
       {buttons.map((button, index) => (
-        <Link to={button.to} key={index}>
-          <Button key={index} styleName={btn_style}>{button.text}</Button>
-        </Link>
+        <Button key={index} styleName={btn_style}>{button.text}</Button>
       ))}
     </div>
   );
