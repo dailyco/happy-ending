dfdd76df1577e36b4412cb5361bc95354beb1113
YuJin-Kim <21700140@handong.edu>
2020-10-20 22:43:16 +0900

    refactoring for pagenation

--- happy-ending/src/ui/Molecules/SelectionBox.js
+++ happy-ending/src/ui/Molecules/SelectionBox.js
@@ -4,30 +4,30 @@ import Button from "../Atoms/Button";
 
 function SelectionBox({ data, styleName }) {
   const history = useHistory();
-  const { to, buttons } = data;
+  const { buttons } = data;
   const { btn_style } = styleName;
   const selectionBoxRef = useRef();
 
   useEffect(() => {
-    selectionBoxRef.current.addEventListener('click', clickHandler)
-    return () => selectionBoxRef.current.removeEventListener('click', clickHandler)
+    selectionBoxRef.current.addEventListener('click', clickHandler);
+    return () => selectionBoxRef.current.removeEventListener('click', clickHandler);
   }, []);
   
   function clickHandler(e) {
-    e.preventDefault()
-    e.target.classList.add('selected')
+    e.preventDefault();
+    e.target.classList.add('selected');
     Array.from(e.currentTarget.querySelectorAll('.Button')).filter(
-      (b) => !b.classList.contains('selected')).forEach(b => b.classList.add('unselected'))
+      (b) => !b.classList.contains('selected')).forEach(b => b.classList.add('unselected'));
 
     e.target.addEventListener('animationend', () => {
-      history.push(to)
+      history.push(e.target.attributes.to.value);
     })
   }
 
   return (
     <div className={"SelectionBox"} ref={selectionBoxRef}>
       {buttons.map((button, index) => (
-        <Button key={index} styleName={btn_style}>{button.text}</Button>
+        <Button key={index} styleName={btn_style} to={button.to}>{button.text}</Button>
       ))}
     </div>
   );
