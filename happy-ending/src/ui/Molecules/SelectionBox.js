96315baf256da482f92acc9055138bf6ba1db5ee
Kim Seokjin <kimseokjin@Kimui-MacBookPro.local>
2020-10-23 19:43:19 +0900

    p8 상태 관리 구현

--- happy-ending/src/ui/Molecules/SelectionBox.js
+++ happy-ending/src/ui/Molecules/SelectionBox.js
@@ -9,27 +9,30 @@ function SelectionBox({ data, styleName }) {
   const selectionBoxRef = useRef();
 
   useEffect(() => {
-    selectionBoxRef.current.addEventListener('click', clickHandler);
-    return () => selectionBoxRef.current.removeEventListener('click', clickHandler);
+    selectionBoxRef.current.addEventListener("click", clickHandler);
+    return () => selectionBoxRef.current.removeEventListener("click", clickHandler);
   }, []);
-  
+
   function clickHandler(e) {
     e.preventDefault();
-    if (e.target.classList.contains('Button')) {
-      e.target.classList.add('selected');
-      Array.from(e.currentTarget.querySelectorAll('.Button')).filter(
-        (b) => !b.classList.contains('selected')).forEach(b => b.classList.add('unselected'));
-      
-      e.target.addEventListener('animationend', () => {
+    if (e.target.classList.contains("Button")) {
+      e.target.classList.add("selected");
+      Array.from(e.currentTarget.querySelectorAll(".Button"))
+        .filter((b) => !b.classList.contains("selected"))
+        .forEach((b) => b.classList.add("unselected"));
+
+      e.target.addEventListener("animationend", () => {
         history.push(e.target.attributes.to.value);
-      })
+      });
     }
   }
 
   return (
     <div className={"SelectionBox"} ref={selectionBoxRef}>
       {buttons.map((button, index) => (
-        <Button key={index} styleName={btn_style} to={button.to}>{button.text}</Button>
+        <Button key={index} styleName={btn_style} to={button.to} onClick={button.onClick}>
+          {button.text}
+        </Button>
       ))}
     </div>
   );
