fb7704a9f92c8ac381c3e2952c3f1f2b83e5d70c
Kim Seokjin <kimseokjin@Kimui-MacBookPro.local>
2020-10-28 22:01:24 +0900

    p7 레이아웃 변경, 투명박스 추가, 망치 선택시 애니메이션 재생 후 넘어가기 구현

--- happy-ending/src/ui/Templates/SeparatedTemplate7.js
+++ happy-ending/src/ui/Templates/SeparatedTemplate7.js
@@ -12,14 +12,10 @@ function SeparatedTemplate7({ data, styleName }) {
   return (
     <div className={classNames("SeparatedTemplate7", tp_style)}>
       <Dquestion data={dq_data} styleName={dq_style}></Dquestion>
-      <div className={classNames("SpanPair")}>
-        <Link to={s1_data.to}>
-          <Span styleName={s1_style}>{s1_data.text}</Span>
-        </Link>
-        <Link to={s2_data.to}>
-          <Span styleName={s2_style}>{s2_data.text}</Span>
-        </Link>
-      </div>
+      <Span styleName={s1_style}>{s1_data.text}</Span>
+      <Link to={s1_data.to} onClick={s1_data.onClick}></Link>
+      <Span styleName={s2_style}>{s2_data.text}</Span>
+      <Link to={s2_data.to}></Link>
     </div>
   );
 }
