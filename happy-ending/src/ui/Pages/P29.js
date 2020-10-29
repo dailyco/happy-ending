81d0d4c491bb01646c594a35da954657993744f8
Kim Seokjin <kimseokjin@Kimui-MacBookPro.local>
2020-10-29 14:56:56 +0900

    data 정리 완료

--- happy-ending/src/ui/Pages/P29.js
+++ happy-ending/src/ui/Pages/P29.js
@@ -15,17 +15,60 @@ function P29() {
     return fInKorean;
   };
 
+  const makeRestText = (choice) => {
+    let restText = "";
+    const travelTo = localStorage.getItem("travelTo") ?? "";
+    const travelWith = localStorage.getItem("travelWith") ?? "";
+    const homeDoWhat = localStorage.getItem("homeDoWhat") ?? "";
+    const homeDoWith = localStorage.getItem("homeDoWith") ?? "";
+
+    if (choice === "travel_alone")
+      restText =
+        travelTo + "(으)로 혼자 여행을 떠나고 싶어했던 당신, 잠시 모든걸 내려놓고 혼자 여행을 떠나보는건 어떤가요? 새로운 곳에서의 신선한 경험들은 길었던 당신의 일상에 쉼표가 되어줄 것입니다.";
+
+    if (choice === "travel_together")
+      restText =
+        travelTo +
+        "(으)로 " +
+        travelWith +
+        "와(과) 함께 여행을 떠나고 싶어했던 당신, 소중한 사람들과 함께 잠시 여행을 떠나보는건 어떤가요? 사랑하는 사람들과 쌓는 추억은 앞으로의 시간들을 살아갈 원동력이 될 것입니다.";
+
+    if (choice === "home_alone")
+      restText =
+        "혼자 집에 남아 " +
+        homeDoWhat +
+        "하고 싶어했던 당신, 잠시 모든 짐을 내려놓고 아늑한 집에서 휴식을 취해보는 건 어떤가요? 혼자만의 시간을 통해 다시한번 삶의 이유와 내가 진정으로 추구해야할 것들을 정리할 수 있을 것입니다.";
+
+    if (choice === "home_together")
+      restText =
+        homeDoWith +
+        "와(과) 함께 집에 남아 " +
+        homeDoWhat +
+        "하고 싶어했던 당신, 오늘은 소중한 사람들과 함께 저녁을 먹으며 시간을 보내는 것은 어떤가요? 진정한 행복과 힐링은 특별한 곳이 아닌 일상 속에서 찾을 수 있습니다.";
+
+    return restText;
+  };
+
   const name = localStorage.getItem("name") ?? "홍길동";
+  const selfie = JSON.parse(localStorage.getItem("photo"));
   const flower = localStorage.getItem("flower") ?? "rose";
-  const flowerInKorean = translateFlower(flower);
+  const travelOrHome = localStorage.getItem("choice_p8").startsWith("travel") ? "travel" : "home";
   const deathMonthNDay = localStorage.getItem("deathMonth") + "월 " + localStorage.getItem("deathday") + "일";
-  const sadMan = localStorage.getItem("sadMan");
+  const alias = localStorage.getItem("alias") ?? "나보다 남을 더 사랑했던";
+  const lastWord = localStorage.getItem("lastWord") ?? "서비스를 마지막으로 나는 떠난다";
+  const flowerInKorean = translateFlower(flower);
+  const happyMoment = localStorage.getItem("happyMoment") ?? "주님을 만났던 순간";
+  const sadMan = localStorage.getItem("sadMan") ?? "김철수";
+  const restText = makeRestText(localStorage.getItem("choice_p8"));
+  const to = JSON.parse(localStorage.getItem("messages"))
+    .map((e) => e.to)
+    .join(", ");
 
   const data = {
     deco_data: {
-      selfie: localStorage.getItem("photo"), // Json.parse로 이미지 객체화하기
+      selfie: selfie,
       flower: flower,
-      travelOrNot: localStorage.getItem("choice_p8") ?? "travel", // startsWith() 사용해서 travel, home 구분
+      travelOrHome: travelOrHome,
     },
     title_data: {
       writer: name + "의",
@@ -34,12 +77,11 @@ function P29() {
     left_page_date: {
       grage_paragraph: {
         grave_header: name + "의 묘",
-        // TODO name, deathMonthNDay, lastWord 넣기
         grave_body: (
           <>
-            박한동은 11월 10일 세상을 떠났습니다.
+            {name}은 {deathMonthNDay} 세상을 떠났습니다.
             <br />
-            박한동의 죽음을 추모하는 사람들이 그의 묘비 앞에서 박한동을 추억합니다. 나보다 남을 더 사랑했던 사람 박한동은 서비스를 마지막으로 나는 떠난다 라는 한마디를 남겼습니다.
+            {name}의 죽음을 추모하는 사람들이 그의 묘비 앞에서 {name}을 추억합니다. {alias} 사람 {name}은 {lastWord} 라는 한마디를 남겼습니다.
           </>
         ),
       },
@@ -49,11 +91,10 @@ function P29() {
       },
       happy_moment_paragraph: {
         happy_moment_header: name + "의 행복했던 순간",
-        // TODO name, happyMoment 넣기
         happy_moment_body: (
           <>
-            박한동은 주님을 만났던 순간에 행복함을 만끽했습니다.
-            <br />그 순간, 박한동은 세상 누구보다도 행복한 사람이라고 느꼈습니다.
+            {name}은 {happyMoment}에 행복함을 만끽했습니다.
+            <br />그 순간, {name}은 세상 누구보다도 행복한 사람이라고 느꼈습니다.
           </>
         ),
       },
@@ -69,14 +110,14 @@ function P29() {
       },
       rest_paragraph: {
         rest_header: "일상의 쉼표",
-        // TODO 이거 사용자 선택에 따라 나누기
-        rest_body: "영국으로 혼자 여행을 떠나고 싶어했던 당신, 잠시 모든걸 내려놓고 혼자 여행을 떠나보는건 어떤가요? 새로운 곳에서의 신선한 경험들은 길었던 당신의 일상에 쉼표가 되어줄 것입니다.",
+        rest_body: restText,
       },
       will_paragraph: {
         will_header: "전하고 싶은 말",
-        // TODO messages에서 name들 가져오기
         will_body:
-          "당신은 죽기전 OOO, OOO, OOO에게 유서를 남겼습니다. 그곳에 적힌 이야기는 당신이 정말로 전하고 싶은 말일 것입니다. 조금은 부끄러운 마음에 말하지 못했다면 이번엔 용기를 내어 직접 전달하거나 작은 행동으로 표현하세요. 계속 미루다보면 표현할 기회를 놓칠 수도 있습니다. ",
+          "당신은 죽기전 " +
+          to +
+          "에게 유서를 남겼습니다. 그곳에 적힌 이야기는 당신이 정말로 전하고 싶은 말일 것입니다. 조금은 부끄러운 마음에 말하지 못했다면 이번엔 용기를 내어 직접 전달하거나 작은 행동으로 표현하세요. 계속 미루다보면 표현할 기회를 놓칠 수도 있습니다.",
       },
     },
     detailed_buttons_data: {
