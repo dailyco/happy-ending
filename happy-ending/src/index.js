7d1ea4f692211430ce6b0a40503c3a183bf79bce
YuJin-Kim <21700140@handong.edu>
2020-10-31 22:46:24 +0900

    release for testing

--- happy-ending/src/index.js
+++ happy-ending/src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/happy-ending">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
